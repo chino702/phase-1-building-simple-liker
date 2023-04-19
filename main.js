4241b2becd1281776f64f933a405ba0ee35cc226
chino702 <chinox3702@gmail.com>
2023-04-19 16:52:20 -0700

    building a simple liker

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+const heart = document.querySelector('.like-glyph');
 
+heart.addEventListener('click', function() {
+  // Check if the heart is currently empty or full
+  const isLiked = heart.innerHTML === FULL_HEART;
 
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+  // Send a request to the server to like or unlike the post
+  mimicServerCall()
+    .then(() => {
+      // Update the UI to reflect that the post has been liked or unliked
+      if (isLiked) {
+        heart.innerHTML = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+        console.log('Heart unliked');
+      } else {
+        heart.innerHTML = FULL_HEART;
+        heart.classList.add('activated-heart');
+        console.log('Heart liked');
+      }
+    })
+    .catch(error => {
+      // Show an error message if the request fails
+      const modal = document.getElementById('modal');
+      modal.classList.add('active');
+      const modalMessage = document.getElementById('modal-message');
+      modalMessage.innerHTML = error;
+      setTimeout(() => {
+        modal.classList.remove('active');
+      }, 5000);
+    });
+});
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
